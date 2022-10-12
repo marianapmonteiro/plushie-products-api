34e689ef9975047821cdcc0d5c44c54d62158fbe
Mariana <marianamonteiro0302@gmail.com>
2022-10-11 23:03:21 -0300

    update

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const db = require("./src/models/database");
+const db = require('./src/models/database');
 
-const express = require("express");
+const express = require('express');
 
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
-require("dotenv").config();
+require('dotenv').config();
 
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
-const cartRoutes = require("./src/routes/cart.routes");
-const loginRoutes = require("./src/routes/login.routes");
-const productRoutes = require("./src/routes/product.routes");
-const userRoutes = require("./src/routes/user.routes");
+const cartRoutes = require('./src/routes/cart.routes');
+const loginRoutes = require('./src/routes/login.routes');
+const productRoutes = require('./src/routes/product.routes');
+const userRoutes = require('./src/routes/user.routes');
 
 const app = express();
 
@@ -24,6 +24,6 @@ app.use(loginRoutes);
 app.use(productRoutes);
 app.use(cartRoutes);
 
-app.listen(process.env.PORT, function () {
-  console.log("servidor online");
+app.listen(process.env.PORT || 3000, function () {
+  console.log('servidor online');
 });
